7d252431646b21789c2ad9e1eb84f5eb8fa634c3
Sarah <sarahrosky@gmail.com>
2020-05-08 17:51:32 -0700

    add new AMAZING work

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*----- constants -----*/
+const playerLookup = {
+    '1': 'Player One',
+    '-1': 'Player Two',
+    'null': 'transparent' 
+  };
+
+/*----- app's state (variables) -----*/
+let board;
+let turn;
+let winner;
+
+/*----- cached element references -----*/
+let gameBoard = document.getElementById('board')
+const msgEl = document.getElementById('msg');
+
+/*----- event listeners -----*/
+gameBoard.addEventListener('click', handleBoardClick)
+
+
+/*----- functions -----*/
+init();
+
+function init() {
+    board = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];
+    turn = 1;
+    winner = null;
+    render ();
+}
+
+function render() {
+
+};
